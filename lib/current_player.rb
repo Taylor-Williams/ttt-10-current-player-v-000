d42f6da0a81ed5e19015927c246806594ad870ac
Taylor Williams <taylor@thewilliams.net>
2018-05-02 22:46:59 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,15 @@
+def current_player(board)
+  if (turn_count % 2 == 0) 
+    "X"
+  else 
+    "O"
+  end
+end
+
+def turn_count(board)
+  turns = 0
+  board.each do |space|
+    if space != " " || space != ""
+      turns ++
+    end  
+end
